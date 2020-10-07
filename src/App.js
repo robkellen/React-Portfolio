02e29e26b73f30910c62bebe5c7bd7e70fd3f0b4
Rob Kellen <rob.h.kellen@gmail.com>
2020-10-07 09:59:40 -0700

    added CSS files to each page component.  Created Navbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import {BrowserRouter as Router, Route} from "react-dom-router";
-import Home from "./components/pages/Home";
-import About from "./components/pages/About";
-import Portfolio from "./components/pages/Portfolio";
-import Contact from "./components/pages/Contact";
-
-
+import { BrowserRouter as Router, Route } from 'react-dom-router';
+import Home from '../src/components/pages/Home';
+import About from '../src/components/pages/About';
+import Portfolio from '../src/components/pages/Portfolio';
+import Contact from '../src/components/pages/Contact';
 
 function App() {
   return (
     <Router>
-      
+      <Route exact path='/' component={Home} />
+      <Route exact path='/About' component={About} />
+      <Route exact path='/Portfolio' component={Portfolio} />
+      <Route exact path='/Contact' component={Contact} />
     </Router>
   );
 }
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,64 @@
+import React from "react";
+import "./Navbar.css";
+import { Link, useLocation } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav class="navbar navbar-expand-lg navbar-light trn">
+      <span class="navbar-brand mb-1 h1">
+        <a href="index.html">
+          <img src="./Assets/BrandLogo.png" alt="brand logo" />
+        </a>
+      </span>
+      <button
+        class="navbar-toggler custom-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon custom-toggler"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="nav navbar-nav navbar-right">
+          <li class="nav-item">
+            <Link
+              to="/About"
+              className={
+                location.pathname === "/About" ? "nav-link active" : "nav-link"
+              }
+            >
+              About
+            </Link>
+          </li>
+          <li class="nav-item">
+            <Link
+              to="/Portfolio"
+              className={
+                location.pathname === "/Portfolio"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+            >
+              Portfolio
+            </Link>
+          </li>
+          <li class="nav-item">
+            <Link
+              to="/Contact"
+              className={
+                location.pathname === "/Contact"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+            >
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
