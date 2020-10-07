29458da16eead2ee96905240dbcd1b29fb84a9c8
Rob Kellen <rob.h.kellen@gmail.com>
2020-10-06 22:36:05 -0700

    updated directory organization by moving Home, About, Portfolio, and Contact into pages directory

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import {BrowserRouter as Router, Route} from "react-dom-router";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Portfolio from "./components/pages/Portfolio";
+import Contact from "./components/pages/Contact";
+
 
 
 function App() {
   return (
-    
+    <Router>
+      
+    </Router>
   );
 }
 
