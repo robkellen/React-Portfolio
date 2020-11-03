09cb08fd10e29c53d25e01cfdfee70d3c4a14f55
Rob Kellen <rob.h.kellen@gmail.com>
2020-11-03 15:51:42 -0700

    added project.json file to hold data for all projects to be mapped over and converted to cards in Portfolio secion

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,20 +1,21 @@
 import React from "react";
 import {Col, Card} from "react-bootstrap";
+import "./Project.css"
 
 
 
-function Project() {
+function Project(props) {
   
   return (
     <Col className="col mb-4">
-    <Card className="card h-100">
-      <img src="" className="card-img-top" alt="" />
+    <Card style={{width: "20rem"}}>
+      <img src={props.image} className="card-img-top" alt={props.name} />
       <div className="card-body">
-        <h5 className="card-title">GameTracker</h5>
-        <p className="card-text">
-          This application allows users to maintain a database of their personal games across all platforms.  This is a full-stack web application designed using the MVC paradigm with server-side API. 
-        </p>
-        <a rel="noopener noreferrer" href="https://github.com/robkellen/GameTracker" target="_blank">Link to GameTracker GitHub Repository</a><br></br><a rel="noopener noreferrer" href="https://glacial-taiga-04215.herokuapp.com/" target="_blank">Try out the live version</a>
+        <h5 className="card-title">{props.name}</h5>
+        {/* <p className="card-text">
+          {props.description}
+        </p> */}
+        <a rel="noopener noreferrer" href={props.gitRepo} target="_blank">Link to {props.name} GitHub Repository</a><br></br><a rel="noopener noreferrer" href={props.liveApp} target="_blank">Try out the live version</a>
       </div>
     </Card>
   </Col>
--- src/components/pages/Portfolio/Portfolio.js
+++ src/components/pages/Portfolio/Portfolio.js
@@ -1,25 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
 import Project from "../../Project/Project";
 import { Container, Row, Col } from "react-bootstrap";
-import Navbar from "../../Navbar/Navbar";
-import Footer from "../../Footer/Footer";
+import projects from "../../../projects.json";
 
-function Portfolio() {
-  return (
-    <div id="portfolio" style={{minHeight: "100vh"}}>
-      <Container>
-        <Row id="main-row" className="row justify-content-center">
-          <Col id="main-col" className="col-sm">
-            <div className="main-header">
-              <h1 className="card-title ">My Work:</h1>
-              <h5 className="card-text">Always in progress...</h5>
-            </div>
-            <Project />
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  );
+
+class Portfolio extends Component {
+  // setting this.state.projects to the projects json array
+  state = {
+    projects
+  };
+
+  render() {
+    return (
+      <div id="portfolio" style={{ minHeight: "100vh" }}>
+        <Container>
+          <Row id="main-row" className="row justify-content-center">
+            <Col id="main-col" className="col-sm">
+              <div className="main-header">
+                <h1 className="card-title ">My Work:</h1>
+                <h5 className="card-text">Always in progress...</h5>
+              </div>
+              {this.state.projects.map(project => (
+              <Project 
+                id={project.id}
+                key={project.id}
+                name={project.name}
+                image={project.image}
+                description={project.description}
+                gitRepo={project.gitRepo}
+                liveApp={project.liveApp}
+              />
+              ))}
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default Portfolio;
