02612a0764322208e13087b9a2b64a6fc61fe6f1
Rob Kellen <rob.h.kellen@gmail.com>
2020-11-23 10:44:49 -0700

    formatted title of Portfolio page.  Added fades to projects and title of portfolio page.

--- src/components/pages/Portfolio/Portfolio.js
+++ src/components/pages/Portfolio/Portfolio.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 import Project from "../../Project/Project";
 import { Container, Row, Col } from "react-bootstrap";
+import Fade from "react-reveal";
 import projects from "../../../projects.json";
-import "./Portfolio.css"
+import "./Portfolio.css";
 
 class Portfolio extends Component {
   // setting this.state.projects to the projects json array
@@ -14,11 +15,13 @@ class Portfolio extends Component {
     return (
       <div id="portfolio">
         <Container className="container">
-          <Row id="main-row" className="row d-flex">
+          <Row id="main-row" className="justify-content-center d-flex">
+            <Fade left>
+              <h1 className="card-title ">My Work:</h1>
+            </Fade>
+          </Row>
+          <Fade right>
             <Col id="main-col" className="col-xs">
-              <div className="main-header">
-                <h1 className="card-title ">My Work:</h1>
-              </div>
               <Row id="card-row" className="justify-content-center">
                 {this.state.projects.map((project) => (
                   <Project
@@ -36,7 +39,7 @@ class Portfolio extends Component {
                 ))}
               </Row>
             </Col>
-          </Row>
+          </Fade>
         </Container>
       </div>
     );
